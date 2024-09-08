45f0e8a789d19ba2d5f71e84a8e676511ae116e8
Parralol <110953563+Parralol@users.noreply.github.com>
2024-09-08 18:42:09 -0500

    Add files via upload

---
+++ src/test/java/com/example/h2/ieti/IetiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.h2.ieti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IetiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
